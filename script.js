b403705a5e83e03ac2a8fa9b2059c613928d851b
GUNAPANDI PM <113769754+Harisankar7813@users.noreply.github.com>
2024-10-02 17:23:17 +0530

    first commit

--- script.js
+++ script.js
@@ -66,7 +66,7 @@ ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right'});
 // typed js
 
 const typed = new Typed('.multiple-text', {
-    strings: ['Frontend Developer', 'YouTuber', 'Blogger'],
+    strings: ['Web Developer', 'Frontend Developer', 'Java Developer'],
     typeSpeed:100,
     backSpeed:100,
     backDelay:100,
